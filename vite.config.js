33efa232766e2c488cd28311c3131ab987e7ef94
Wilson Narea <wilsonnarea27@gmail.com>
2025-08-13 17:41:47 -0400

    Configure Vite for GitHub Pages deployment, fix file name issue

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? ''
+const isUserSite = /\.github\.io$/i.test(repo)
+
 export default defineConfig({
   plugins: [react()],
-  // If your repo is e.g. "portfolio-website"
-  base: `/${process.env.GITHUB_REPOSITORY?.split('/')[1] ?? 'portfolio-website'}/`,
+  base: isUserSite ? '/' : `/${repo}/`,
 })
