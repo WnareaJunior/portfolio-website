ace6dfff88fca9862c2a73a877f8e9cfb071a217
Wilson Narea <wilsonnarea27@gmail.com>
2025-08-13 17:37:36 -0400

    Add App.jsx with correct casing

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div>
+      <h1>Hello Vite + React</h1>
+      <p>Your portfolio site is ready to build!</p>
+    </div>
+  )
+}
--- src/app.jsx
+++
@@ -1,5 +0,0 @@
-// src/App.jsx
-export default function App() {
-    return <h1>Hello Vite + React</h1>
-  }
-  
