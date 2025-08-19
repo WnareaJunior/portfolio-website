9f7b6cc974f623de1225e4d08c7855967f31ec31
Wilson Narea <wilsonnarea27@gmail.com>
2025-08-19 19:23:43 -0400

    fixed base path vite config for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  // In dev (`vite`), use root `/`. In builds, read BASE_PATH (set by CI) or default to `/`.
-  base: command === 'serve' ? '/' : (process.env.BASE_PATH || '/'),
+  base:'/'
 }))
